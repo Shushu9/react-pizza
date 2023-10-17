2193d6ad58b56b33a4d8ad7242b817a8c3111f6c
Shushu9 <pokotilo.gleb1@gmail.com>
2023-10-17 22:41:16 +0300

    refactor: add fullizza page types

--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -1,10 +1,15 @@
+import React from 'react';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
 
 const FullPizza = () => {
-    const [pizza, setPizza] = useState();
+    const [pizza, setPizza] = useState<{
+        imageUrl: string;
+        title: string;
+        price: number;
+    }>();
 
     const { id } = useParams();
     const navigate = useNavigate();
@@ -25,7 +30,6 @@ const FullPizza = () => {
     if (!pizza) {
         return <>Загрузка...</>;
     }
-
     return (
         <div className="container">
             <img src={pizza.imageUrl} alt='pizza' />
